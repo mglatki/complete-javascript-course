788a0654d57923ab4d004a95922d3bb856c832ba
mglatki <mglatki@gmail.com>
2021-02-08 23:08:19 +0100

    Variables training

--- 01-Fundamentals-Part-1/starter/main.js
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -1,7 +1,26 @@
 let js = "amazing";
 
-if(js == "amazing"){
-  alert("JS is FUN!!1!11");
-}
-
 console.log(65);
+
+console.log('Jonas');
+
+let firstName = "Michał";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let country = "Poland";
+let continent = "Europe";
+let population = 1000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(language);
